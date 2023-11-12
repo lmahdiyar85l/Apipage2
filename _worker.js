647b78e07d4a8a26522011a5a822f6c1f6eee5f0
lmahdiyar85l <128718326+lmahdiyar85l@users.noreply.github.com>
2023-11-12 16:35:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -128,7 +128,7 @@ export default {
 					default:
 						// return new Response('Not found', { status: 404 });
 						// For any other path, reverse proxy to 'www.fmprc.gov.cn' and return the original response
-						url.hostname = Math.random() < 0.5 ? 'www.gov.cn' : 'www.fmprc.gov.cn';
+						url.hostname = Math.random() < 0.5 ? 'panel.arvancloud.ir' : 'arvancloud.ir';
 						url.protocol = 'https:';
 						request = new Request(url, request);
 						return await fetch(request);
